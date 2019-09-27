b35439ca55b746515da864e5fe13564a118902cf
ickeith <ksummers@icstars.org>
2019-09-27 14:45:42 -0500

    Updates 1

--- src/client/App.js
+++ src/client/App.js
@@ -3,19 +3,29 @@ import './app.css';
 import ReactImage from './react.png';
 
 export default class App extends Component {
-  state = { username: null };
+  state = { 
+    username: null,
+    text: '',
+    res: null,
+     };
 
   componentDidMount() {
     fetch('/api/getUsername')
       .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+      .then(data => this.setState({ 
+        username: data.username,
+      text: data.text,
+      res: data.data
+     }));
   }
 
   render() {
-    const { username } = this.state;
+    const { username, text, res } = this.state;
+
+    console.log(res);
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+        {username ? <h1>{`${text} ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
         <img src={ReactImage} alt="react" />
       </div>
     );
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,55 @@
+require('dotenv').config();
 const express = require('express');
 const os = require('os');
+const fetch = require('node-fetch');
 
 const app = express();
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+const API_KEY = process.env.API_KEY;
+const API_URL = process.env.API_URL;
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+console.log(API_URL);
+
+app.use(express.static('dist'));
+app.get('/api/getUsername', function (req,res, next){
+        let myResponse = {
+            username: os.userInfo().username,
+            text: 'Bye'
+        }
+
+        fetch(`${API_URL}?api_key=${API_KEY}&query=spiderman`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+
+                myResponse.data = data;
+                
+                res.send(myResponse)
+            });
+            
+    
+
+
+    // fetch('/api/getUsername')
+    //   .then(res => res.json())
+    //   .then(data => this.setState({ 
+    //     username: data.username,
+    //   text: data.text,
+    //   res: data.res
+    //  }));
+
+
+    //  function requestMovies(url, onComplete, onError) {
+    //     fetch(url)
+    //         .then((res) => res.json())
+    //         .then(onComplete)
+    //         .catch(onError);
+    // }
+
+
+});
+
+
+// app.use(express.static('dist'));
+// app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+ app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
